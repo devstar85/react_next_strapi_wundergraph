900e7bc39378e0d6c797fb0027d8006145df6fe1
topdev2985 <smartdevtop1989@gmail.com>
2023-02-16 06:03:57 +0900

    update wundergraph

--- .wundergraph/wundergraph.config.ts
+++ .wundergraph/wundergraph.config.ts
@@ -1,11 +1,12 @@
 import { configureWunderGraphApplication, cors, EnvironmentVariable, introspect, templates } from '@wundergraph/sdk';
+import { NextJsTemplate } from '@wundergraph/nextjs/dist/template';
 import server from './wundergraph.server';
 import operations from './wundergraph.operations';
 
 const strapi = introspect.graphql({
-apiNamespace: 'backend',
-url: 'http://localhost:1337/graphql',
-})  
+	apiNamespace: 'backend',
+	url: 'http://localhost:1337/graphql',
+})
 const spaceX = introspect.graphql({
 	apiNamespace: 'spacex',
 	url: 'https://spacex-api.fly.dev/graphql/',
@@ -20,8 +21,8 @@ configureWunderGraphApplication({
 			templates: [...templates.typescript.all],
 		},
 		{
-			templates: [templates.typescript.client],
-			path: '../src/components/generated',
+			templates: [new NextJsTemplate()],
+			path: '../components/generated',
 		},
 	],
 	cors: {
@@ -29,9 +30,9 @@ configureWunderGraphApplication({
 		allowedOrigins:
 			process.env.NODE_ENV === 'production'
 				? [
-						// change this before deploying to production to the actual domain where you're deploying your app
-						'http://localhost:3000',
-				  ]
+					// change this before deploying to production to the actual domain where you're deploying your app
+					'http://localhost:3000',
+				]
 				: ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
 	},
 	security: {
